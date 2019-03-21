d07eea0e50082a59f836e5686b4c237fe7ca5a9f
W.Abdelbaset <W.Abdelbaset@NB-WAbdelbaset.duc.com.kw>
2019-03-21 10:07:24 +0300

    Added

---
+++ Test Suites/Test_Suite 1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>w.abdelbaset@diyarme.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <testSuiteGuid>73233b80-32c7-4546-b7fe-35dddbca1425</testSuiteGuid>
+   <testCaseLink>
+      <guid>33904213-2f0b-4913-8d25-a947d29a092e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Wesam</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ad0ba07-ab20-415d-87ee-33c34f8205aa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 2 by Wesam</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
